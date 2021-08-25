d13d3b84b6b60a7fcd6001ed133b2a231d8b3b17
avesda <49653439+avesda@users.noreply.github.com>
2021-08-25 14:59:22 +0300

    Update scenarios.ts

--- src/scenarios.ts
+++ src/scenarios.ts
@@ -82,9 +82,8 @@ const singlePayTxn: Scenario = async (
     from: address,
     to: testAccounts[0].addr,
     amount: 100000,
-    fee: 2000,
     note: new Uint8Array(Buffer.from("Here is a very big noteeeeeeeeeeeee")),
-   
+    suggestedParams,
   });
 
   const txnsToSign = [{ txn, message: "This is a message from the dApp This is a message from the dApp This is a message from the dApp " }];
